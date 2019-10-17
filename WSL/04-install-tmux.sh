4a3b0a47a35a1511f5c742d2f2a0157ee6593be8
pantasio <hoang.lduc@gmail.com>
2019-10-17 15:49:16 +0700

    Update 04-install-tmux.sh

--- WSL/04-install-tmux.sh
+++ WSL/04-install-tmux.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 # Update the apt package list.
-sudo apt-fast update -y
-sudo apt-fast install -y software-properties-common
+apt-fast update -y
+
+apt-fast install -y wget tar libevent-dev libncurses-dev
+apt-fast install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip gawk
+
+wget https://github.com/tmux/tmux/releases/download/3.0/tmux-3.0-rc5.tar.gz
+tar xf tmux-3.0-rc5.tar.gz
+rm -f tmux-3.0-rc5.tar.gz
+
+cd tmux-3.0-rc5
+./configure
+make
+make install
+cd ~
+rm -rf /usr/local/src/tmux-\*
+mv tmux-3.0-rc5 /usr/local/src
 
