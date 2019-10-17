df6ade9feeba3f9fd0ba7880c8867178892b09f6
pantasio <hoang.lduc@gmail.com>
2019-10-17 15:47:19 +0700

    Update 02-install-fish.sh

--- WSL/02-install-fish.sh
+++ WSL/02-install-fish.sh
@@ -12,5 +12,7 @@ curl -L https://get.oh-my.fish > install-ohmyfish
 fish install-ohmyfish --path=~/.local/share/omf --config=~/.config/omf
 chsh -s /usr/bin/fish
 
+sleep 5
+echo "Install bobthefish"
 omf install bobthefish
 
