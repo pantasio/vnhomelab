8870664c5fdd5044152da84386b8c4ac57b21c3b
pantasio <hoang.lduc@gmail.com>
2019-10-17 15:16:13 +0700

    Update 01-install-apt-fast.sh

--- WSL/01-install-apt-fast.sh
+++ WSL/01-install-apt-fast.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Update the apt package list.
-apt-get update -y
-apt-get install -y software-properties-common git curl wget
-
 add-apt-repository ppa:apt-fast/stable -y
 apt-get update -y
 apt-get install apt-fast -y
