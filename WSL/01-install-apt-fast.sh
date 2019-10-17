c88d6e0b939a28aa16a3a38021dadd69fdfba224
pantasio <hoang.lduc@gmail.com>
2019-10-17 15:40:44 +0700

    Update 01-install-apt-fast.sh

--- WSL/01-install-apt-fast.sh
+++ WSL/01-install-apt-fast.sh
@@ -11,8 +11,12 @@ apt-get install apt-fast -y
 ##############################################
 # MIRRORS=( ‘http://opensource.xtdv.net/ubuntu, http://mirrors.nhanhoa.com/ubuntu, http://mirror.ehost.vn/ubuntu, http://mirror.0x.sg/ubuntu, http://free.nchc.org.tw/ubuntu, http://mirror.xtom.com.hk/ubuntu, http://archive.ubuntu.com/ubuntu’)
 clear
+echo "#########################"
 echo "apt-fast.conf file"
 sleep 3
+
 wget https://raw.githubusercontent.com/pantasio/vnhomelab/master/WSL/apt-fast.conf
 mv ./apt-fast.conf /etc/apt-fast.conf
+
+echo "#########################"
 echo "DONE, apt-fast Install successed"
