b81211a5db17e3331f5220e03d46620799d5254b
pantasio <hoang.lduc@gmail.com>
2019-10-31 12:49:40 +0700

    update

--- WSL/02-install-fish.sh
+++ WSL/02-install-fish.sh
@@ -11,4 +11,10 @@ chsh -s /usr/bin/fish
 curl -L https://get.oh-my.fish > install-ohmyfish
 fish install-ohmyfish --path=~/.local/share/omf --config=~/.config/omf
 chsh -s /usr/bin/fish
-./02a-fish-theme.sh
+
+## Install theme fish [bobthefish]
+
+echo "install Fish DONE. <br>"
+echo "Now we Install bobthefish theme for fishshell. <br>"
+echo "Pls run command <br><br>"
+echo "omf install bobthefish<br><br>"
