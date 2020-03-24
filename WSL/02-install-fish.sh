485a4c8171d0e97626895ef538190b1c5edc1b06
Duc Hoang Le <hoang.lduc@gmail.com>
2020-03-23 20:26:19 -0700

    Update 02-install-fish.sh

--- WSL/02-install-fish.sh
+++ WSL/02-install-fish.sh
@@ -2,18 +2,16 @@
 
 # Install Fish shell
 sudo apt-add-repository ppa:fish-shell/release-3 -y
-sudo apt-fast update -y
 sudo apt-fast install -y fish
-
+chsh -s /usr/bin/fish
 ## install oh-my-fish and theme bobthefish  
 curl -L https://get.oh-my.fish > install-ohmyfish
-sleep 1
+sleep 0.1
 
+chmod +x install-ohmyfish
 fish install-ohmyfish --path=~/.local/share/omf --config=~/.config/omf
-sleep 1
+sleep 0.1
 
-chsh -s /usr/bin/fish
-sleep 1
 
 ## Install theme fish [bobthefish]
 clear
